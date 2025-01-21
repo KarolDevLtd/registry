2802f31da674c1a76d71ddea011582e801331bd4
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2025-01-21 17:15:43 +0000

    Update method and unit tests

--- contracts/src/First.test.ts
+++ contracts/src/First.test.ts
@@ -1,4 +1,12 @@
-import { AccountUpdate, Mina, PrivateKey, PublicKey, Cache } from 'o1js';
+import {
+  AccountUpdate,
+  Mina,
+  PrivateKey,
+  PublicKey,
+  Cache,
+  Field,
+  Signature,
+} from 'o1js';
 import { First } from './First';
 
 /*
@@ -64,5 +72,81 @@ describe('First', () => {
 
     const storedAdminKey = await zkApp.adminKey.fetch();
     expect(storedAdminKey).toEqual(adminPublicKey);
+  });
+
+  it('verifies admin signature correctly', async () => {
+    await localDeploy();
+
+    //initWorld setup
+    const adminPrivateKey = PrivateKey.random();
+    const adminPublicKey = adminPrivateKey.toPublicKey();
+    const initTxn = await Mina.transaction(senderAccount, async () => {
+      await zkApp.initWorld(adminPublicKey);
+    });
+    await initTxn.prove();
+    await initTxn.sign([senderKey]).send();
+
+    //verifyAdmin setup
+    const message = Field(1);
+    const signature = Signature.create(adminPrivateKey, [message]);
+    const verifyTxn = await Mina.transaction(senderAccount, async () => {
+      await zkApp.verifyAdmin(message, signature);
+    });
+    await verifyTxn.prove();
+    await verifyTxn.sign([senderKey]).send();
+
+    // If no error, pass
+    expect(true).toBe(true);
+  });
+
+  it('updates value with valid admin signature', async () => {
+    await localDeploy();
+
+    //initWorld setup
+    const adminPrivateKey = PrivateKey.random();
+    const adminPublicKey = adminPrivateKey.toPublicKey();
+    const initTxn = await Mina.transaction(senderAccount, async () => {
+      await zkApp.initWorld(adminPublicKey);
+    });
+    await initTxn.prove();
+    await initTxn.sign([senderKey]).send();
+
+    //updateValue setup
+    const newValue = Field(1);
+    const message = Field(999);
+    const signature = Signature.create(adminPrivateKey, [message]);
+    const updateTxn = await Mina.transaction(senderAccount, async () => {
+      await zkApp.updateValue(newValue, message, signature);
+    });
+    await updateTxn.prove();
+    await updateTxn.sign([senderKey]).send();
+    const storedValue = await zkApp.value.fetch();
+    expect(storedValue).toEqual(newValue);
+  });
+
+  it('fails to update value with invalid admin signature', async () => {
+    await localDeploy();
+
+    //initWorld setup
+    const adminPrivateKey = PrivateKey.random();
+    const adminPublicKey = adminPrivateKey.toPublicKey();
+    const initTxn = await Mina.transaction(senderAccount, async () => {
+      await zkApp.initWorld(adminPublicKey);
+    });
+    await initTxn.prove();
+    await initTxn.sign([senderKey]).send();
+
+    //updateValue setup
+    const newValue = Field(1);
+    const message = Field(999);
+    const invalidPrivateKey = PrivateKey.random();
+    const invalidSignature = Signature.create(invalidPrivateKey, [message]);
+    await expect(async () => {
+      const updateTxn = await Mina.transaction(senderAccount, async () => {
+        await zkApp.updateValue(newValue, message, invalidSignature);
+      });
+      await updateTxn.prove();
+      await updateTxn.sign([senderKey]).send();
+    }).rejects.toThrow();
   });
 });
--- contracts/src/First.ts
+++ contracts/src/First.ts
@@ -7,17 +7,19 @@ import {
   Signature,
   Field,
   AccountUpdate,
+  UInt64,
 } from 'o1js';
 
 export class First extends SmartContract {
   // State variable to store the admin's public key
   @state(PublicKey) adminKey = State<PublicKey>();
-  // @state(Field) state2 = State<Field>();
+  @state(Field) value = State<Field>();
 
   // Method to initialize the admin key
   @method async initWorld(adminPublicKey: PublicKey) {
     super.init(); // Initialize the contract
     this.adminKey.set(adminPublicKey); // Store the admin's public key
+    this.value.set(Field(0));
   }
 
   // verify ownership using a signature
@@ -34,5 +36,22 @@ export class First extends SmartContract {
     isValidSignature.assertTrue('Valid signature');
     // this.state2.set();
     // AccountUpdate.create(this.sender).update.appState[1].value;
+  }
+
+  @method async updateValue(
+    newValue: Field,
+    message: Field,
+    signature: Signature
+  ) {
+    await this.verifyAdmin(message, signature);
+
+    let paymentAmount = UInt64.from(2500000000);
+
+    let senderUpdate = AccountUpdate.createSigned(
+      this.adminKey.getAndRequireEquals()
+    );
+    senderUpdate.send({ to: this, amount: paymentAmount });
+
+    this.value.set(newValue);
   }
 }
